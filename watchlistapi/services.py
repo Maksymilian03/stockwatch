88bd3c28eadda5803e8fa34c0137486ab1e0b35a
Maksymilian Stolarek <maksiu2003xd@gmail.com>
2026-05-04 18:26:51 +0200

    Add Alpha Vantage integration with current and historical price endpoints

--- watchlistapi/services.py
+++ watchlistapi/services.py
@@ -23,3 +23,23 @@ def get_stock_price(symbol):
     else:
         
        return price
+
+
+def get_stock_price_at_date(symbol, date):
+    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={API_KEY}'
+    try:
+        response = requests.get(url)
+        data = response.json()
+
+        price_at_end_of_day = data['Time Series (Daily)'][date]['4. close']
+    except (KeyError, ConnectionError):
+        return None
+    else:
+        return price_at_end_of_day
+
+
+
+if __name__ == '__main__':
+    
+
+    print(get_stock_price_at_date('AAPL', '2026-05-04'))
--- watchlistapi/views.py
+++ watchlistapi/views.py
@@ -6,7 +6,7 @@ from .serializers import StockSerializer
 from rest_framework import generics
 from .serializers import RegisterSerializer
 from rest_framework.permissions import AllowAny
-from .services import get_stock_price
+from .services import get_stock_price, get_stock_price_at_date
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import status
@@ -35,7 +35,17 @@ class StockViewSet(viewsets.ModelViewSet):
         price = get_stock_price(ticker.symbol)
         if price is None:
              return Response({'error': 'Nie można pobrać ceny dla tego tickera'}, status=status.HTTP_404_NOT_FOUND)
-        return Response({'price': price})
+        return Response({'price': price, 'ticker': ticker.name})
+
+    @action(detail=True, methods=['get'])
+    def price_at_date(self, request, pk=None):
+        ticker = Stock.objects.get(pk=pk, user=self.request.user)
+        date = ticker.add_date.strftime('%Y-%m-%d')
+        price = get_stock_price_at_date(ticker.symbol, date)
+
+        if price is None:
+            return Response({'error': 'Nie można pobrać ceny dla tego tickera'}, status=status.HTTP_404_NOT_FOUND)
+        return Response({'price': price, 'ticker': ticker.name})
 
 
 class RegisterView(generics.CreateAPIView):
